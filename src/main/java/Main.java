af7d2b97b9680a2fb697762a621667046683c3b5
Marcin Tlon <marcin.tlon1@gmail.com>
2022-06-07 11:44:05 +0200

    Main commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Email em1 = new Email("Marcin", "Tlon");
+            em1.department();
+            System.out.println(em1.password(8));
+        em1.email();
+    }
+}
